d8a5037e92fecb84382644c2db025c7ab73cb8bc
Calli Oberembt <croberembt@gmail.com>
2020-10-02 12:27:25 -0500

    using oauth with passport and facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '385265392866312',
+        clientSecret: 'c6d2dafaee1bcf0537d69527cc056299'
+    }
 }
